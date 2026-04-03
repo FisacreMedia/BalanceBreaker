c794ba9709560a9189889f7e289a8efc4c573091
Jordan Fazakerley <fisacremedia@gmail.com>
2026-04-03 00:40:43 +0000

    +

---
+++ Scripts/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 4.5
+@export var input_left : String = "ui_left"
+@export var input_right : String = "ui_right"
+@export var input_forward : String = "ui_up"
+@export var input_backward : String = "ui_down"
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector(input_left, input_right, input_forward, input_backward)
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
