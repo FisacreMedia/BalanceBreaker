824a946d60cb740f75d8732e6d3b97427837e07c
Jordan Fazakerley <fisacremedia@gmail.com>
2026-04-06 23:36:32 +0000

    Changes to be committed: 	modified:   Scripts/player.gd 	modified:   node_3d.tscn  Untracked files: 	Scripts/NPC.gd 	Scripts/NPC.gd.uid 	Scripts/character_model.gd 	Scripts/character_model.gd.uid 	Scripts/follow_camera.gd 	Scripts/follow_camera.gd.uid

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,17 +1,18 @@
 extends CharacterBody3D
 
-
 @export var SPEED = 5.0
-@export var JUMP_VELOCITY = 4.5
+@export var ROTATION_SPEED = 0.5
 @export var input_left : String = "ui_left"
 @export var input_right : String = "ui_right"
 @export var input_forward : String = "ui_up"
 @export var input_backward : String = "ui_down"
 
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,5 +24,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
+	
 	move_and_slide()
