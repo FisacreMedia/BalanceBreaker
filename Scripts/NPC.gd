4bfae62e4eb59a714c0efa556792232a45353ae1
Jordan Fazakerley <fisacremedia@gmail.com>
2026-04-06 23:38:30 +0000

    Updated and added new scripts.  Changes to be committed: 	new file:   Scripts/NPC.gd 	new file:   Scripts/NPC.gd.uid 	new file:   Scripts/character_model.gd 	new file:   Scripts/character_model.gd.uid 	new file:   Scripts/follow_camera.gd 	new file:   Scripts/follow_camera.gd.uid

---
+++ Scripts/NPC.gd
@@ -0,0 +1,16 @@
+class_name NPC
+
+extends CharacterBody3D
+
+enum Alignment {ALLY, NEUTRAL, HOSTILE }
+@export var unit_alignment : Alignment
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
