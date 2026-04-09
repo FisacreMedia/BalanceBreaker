b6fe6d711b3d8848092ab1c8672e403361f2f2ea
Jordan Fazakerley <fisacremedia@gmail.com>
2026-04-09 16:54:51 +0000

    Changes to be committed: 	modified:   Scripts/NPC.gd 	new file:   Scripts/interact_prompt.gd 	new file:   Scripts/interact_prompt.gd.uid 	modified:   Scripts/player.gd 	new file:   Scripts/ray_cast_3d.gd 	new file:   Scripts/ray_cast_3d.gd.uid 	new file:   Scripts/user_interface.gd 	new file:   Scripts/user_interface.gd.uid 	modified:   node_3d.tscn 	new file:   user_interface.tscn

--- Scripts/NPC.gd
+++ Scripts/NPC.gd
@@ -4,7 +4,9 @@ extends CharacterBody3D
 
 enum Alignment {ALLY, NEUTRAL, HOSTILE }
 @export var unit_alignment : Alignment
-
+@onready var player = $"../Player"
+@onready var interact_raycast = $InteractRayCast
+signal interact_ready
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +15,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if interact_raycast.is_colliding():
+		print("Player Detected")
+		interact_ready.emit()
+		
 	pass
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,9 +6,22 @@ extends CharacterBody3D
 @export var input_right : String = "ui_right"
 @export var input_forward : String = "ui_up"
 @export var input_backward : String = "ui_down"
+@export var sensitivity = 100
+@onready var camera = $"../Camera3D"
 
 
 func _physics_process(delta: float) -> void:
+	var ground_plane = Plane(Vector3.UP, global_position.y)
+	var mouse_pos = get_viewport().get_mouse_position()
+	
+	var ray_origin = camera.project_ray_origin(mouse_pos)
+	var ray_direction = camera.project_ray_normal(mouse_pos)
+	
+	var intersection = ground_plane.intersects_ray(ray_origin, ray_direction)
+	
+	if intersection:
+		look_at(Vector3(intersection.x, global_position.y, intersection.z), Vector3.UP)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
